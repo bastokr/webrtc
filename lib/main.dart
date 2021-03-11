0671152c385be703ff67b1485c06172813ddb3a6
skpark <you@example.com>
2021-03-11 13:14:20 +0900

    20210311

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application. 20210311
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
